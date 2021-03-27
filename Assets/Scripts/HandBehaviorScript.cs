3a75ddecad4efb9d4be5ad159b6f05c991f294cc
Qapples <34142272+Qapples@users.noreply.github.com>
2021-03-27 19:36:00 -0400

    Add hand slam ability

--- Assets/Scripts/HandBehaviorScript.cs
+++ Assets/Scripts/HandBehaviorScript.cs
@@ -1,36 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.IO.Ports;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
-using UnityEngine.Serialization;
 
 public class HandBehaviorScript : MonoBehaviour
 {
-    [Header("Serial Communication")]
-    [Tooltip("Com port of the device to communicate to")]
+    /// <summary>
+    /// Debug mode. Takes input from keyboard instead of serial ports.
+    /// </summary>
+    [Header("Debug Mode")] public bool debug;
+    
+    [Header("Serial Communication")] [Tooltip("Com port of the device to communicate to")]
     public int comPort;
 
     [Tooltip("Baud rate of communication")]
     public int baudRate;
+
+    [Header("Hand Properties")] [Tooltip("How fast the hand goes up when it tries to recover")]
+    public float recoverySpeed;
+
+    [Tooltip("How fast the hand slams down")]
+    public float slamSpeed;
     
     private SerialPort _serialPort;
-    
+    private bool _isSlamReady;
+    private float _originalYPos;
+    private Rigidbody _rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Make sure that the time scale is 1. It could be zero if it the previous game was a game over.
+        Time.timeScale = 1;
+        _originalYPos = transform.position.y;
+        _rigidBody = GetComponent<Rigidbody>();
+        _isSlamReady = true;
+        
+        //ensure that the slam value is negative
+        slamSpeed = -Math.Abs(slamSpeed);
+        
+        //Serial setup. Don't do if in debug mode
+        if (debug) return;
         _serialPort = new SerialPort {PortName = $"COM{comPort}", BaudRate = baudRate};
-
         _serialPort.Open();
     }
 
     // Update is called once per frame
     void Update()
     {
-        string data = _serialPort.ReadLine();
-        if (data.Length > 0)
+        //If the hand reaches it's "recovery position" after slamming, get ready for another slam
+        if (!_isSlamReady && transform.position.y >= _originalYPos)
         {
-            Debug.Log(data);
+            _isSlamReady = true;
+            _rigidBody.velocity = Vector3.zero;
+        }
+        
+        //slam the hand if the button is pressed and the hand has recovered
+        if (SlamHand && _isSlamReady)
+        {
+            _rigidBody.velocity = new Vector3(0, slamSpeed, 0);
+            _isSlamReady = false;
         }
     }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        //if we collided with the bug_, level up. If collided with the floor, begin recovery. If collided
+        //with anything else, we lose
+        if (other.gameObject.name == "Bug")
+        {
+            GlobalVar.CurrentLevel++;
+        }
+        else if (other.gameObject.name == "Floor")
+        {
+            //send the hand back up
+            _rigidBody.velocity = new Vector3(0, recoverySpeed, 0);
+        }
+        else //Game over.
+        { 
+            //TODO: Add more game lose logic
+            Debug.Log("Game Over");
+
+            //set the time scale as zero for now. 
+            Time.timeScale = 0;
+        }
+    }
+
+    /// <summary>
+    /// Determines if the hand should be slammed down. 
+    /// </summary>
+    /// <returns></returns>
+    private bool SlamHand => debug ? Input.GetButtonDown("Jump") : _serialPort.ReadLine()[0] == '1';
 }
